80fe78774e9e507e3cba4202d404eeb3dfc2b004
MNzorubara <maxime.nzorubara@gmail.com>
2021-04-22 01:27:25 +0200

    graphic pages

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # EEC4TBX-Heroku
 
 For the static page design and test
+
+## Avant de lancer le serveur verifier l'installation de l'ensemble de dependencies
+
+```
+pip install -r requirements.txt
+```
--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from django.template import loader
+
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("Creation of a MVC generated Page")
+    variable = "EEC4TBX Page"
+    template = loader.get_template('polls/index.html')
+    context = {
+        'variable': variable,
+        'STATIC_URL': "static/",
+    }
+    return HttpResponse(template.render(context,request))
