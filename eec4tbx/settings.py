771d95e871b4f0ae4f2bd49548368b030e9ef17c
MaximeNzo <58881605+MaximeNzo@users.noreply.github.com>
2021-05-08 17:58:41 +0200

    Update settings.py

--- eec4tbx/settings.py
+++ eec4tbx/settings.py
@@ -76,6 +76,7 @@ WSGI_APPLICATION = 'eec4tbx.wsgi.application'
 
 DATABASES = {
     'default': {
+        'CONN_MAX_AGE': 500
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
